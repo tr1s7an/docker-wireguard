f1c61c53933187714ee11823f1c09f603f00eafb
tr1s7an <50695187+tr1s7an@users.noreply.github.com>
2022-08-01 09:29:39 +0800

    Use ip(6)tables-nft

--- setup-wireguard.sh
+++ setup-wireguard.sh
@@ -1,13 +1,13 @@
 #/bin/sh
 
-ping -c 1 -6 www.google.com
+ping -c 1 -6 www.cloudflare.com
 if [ ${?} -eq 2 ]; then
 cat << EOF > /etc/wireguard/${interface_name}.conf
 [Interface]
   PrivateKey = ${serverprikey} 
   Address    = 10.10.10.1/24
-  PostUp     = iptables -A FORWARD -i ${interface_name} -j ACCEPT; iptables -A FORWARD -o ${interface_name} -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE;
-  PostDown   = iptables -D FORWARD -i ${interface_name} -j ACCEPT; iptables -D FORWARD -o ${interface_name} -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE;
+  PostUp     = iptables-nft -A FORWARD -i ${interface_name} -j ACCEPT; iptables-nft -A FORWARD -o ${interface_name} -j ACCEPT; iptables-nft -t nat -A POSTROUTING -o eth0 -j MASQUERADE;
+  PostDown   = iptables-nft -D FORWARD -i ${interface_name} -j ACCEPT; iptables-nft -D FORWARD -o ${interface_name} -j ACCEPT; iptables-nft -t nat -D POSTROUTING -o eth0 -j MASQUERADE;
   ListenPort = 44444
   DNS        = 1.0.0.1
   MTU        = 1420
@@ -20,8 +20,8 @@ cat << EOF > /etc/wireguard/${interface_name}.conf
 [Interface]
   PrivateKey = ${serverprikey} 
   Address    = 10.10.10.1/24,fdff::1/64
-  PostUp     = iptables -A FORWARD -i ${interface_name} -j ACCEPT; iptables -A FORWARD -o ${interface_name} -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -A FORWARD -i ${interface_name} -j ACCEPT; ip6tables -A FORWARD -o ${interface_name} -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE;
-  PostDown   = iptables -D FORWARD -i ${interface_name} -j ACCEPT; iptables -D FORWARD -o ${interface_name} -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -D FORWARD -i ${interface_name} -j ACCEPT; ip6tables -D FORWARD -o ${interface_name} -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE;
+  PostUp     = iptables-nft -A FORWARD -i ${interface_name} -j ACCEPT; iptables-nft -A FORWARD -o ${interface_name} -j ACCEPT; iptables-nft -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables-nft -A FORWARD -i ${interface_name} -j ACCEPT; ip6tables-nft -A FORWARD -o ${interface_name} -j ACCEPT; ip6tables-nft -t nat -A POSTROUTING -o eth0 -j MASQUERADE;
+  PostDown   = iptables-nft -D FORWARD -i ${interface_name} -j ACCEPT; iptables-nft -D FORWARD -o ${interface_name} -j ACCEPT; iptables-nft -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables-nft -D FORWARD -i ${interface_name} -j ACCEPT; ip6tables-nft -D FORWARD -o ${interface_name} -j ACCEPT; ip6tables-nft -t nat -D POSTROUTING -o eth0 -j MASQUERADE;
   ListenPort = 44444
   DNS        = 1.0.0.1,2606:4700:4700::1001
   MTU        = 1420
